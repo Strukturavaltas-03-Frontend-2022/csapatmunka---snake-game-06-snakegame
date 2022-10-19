b5ba78345627bcddeb8bac9a06bc6269f4028400
k3csk3 <szilagyi.viktor89@gmail.com>
2022-10-19 17:23:40 +0200

    utils done

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,50 +1,52 @@
-import { SIZE } from './constants';
+import { SIZE } from "./constants";
 
 class Debouncer {
+  /**
+   * FELADAT!
+   * Hozd létre a statikus bound metódust!
+   * @param num {number}
+   * @param min {number}
+   * @param max {number}
+   * @returns {number} - Válaszd ki a kisebbet a num és a max közül,
+   * majd válaszd ki a nagyobbat az előbbi érték és a min közül.
+   */
+  static bound(num: number, min: number, max: number): number {
+    return Math.max(Math.min(num, max), min);
+  }
 
-    /**
-     * FELADAT!
-     * Hozd létre a statikus bound metódust!
-     * @param num {number}
-     * @param min {number}
-     * @param max {number}
-     * @returns {number} - Válaszd ki a kisebbet a num és a max közül, 
-     * majd válaszd ki a nagyobbat az előbbi érték és a min közül.
-     */
-    
-
-    static debounce<T extends Function>(fun: T, wait: number) {
-        let id: any;
-        return (...args: any[]) => {
-            clearTimeout(id);
-            id = setTimeout(() => fun(...args), wait);
-        };
-    }
+  static debounce<T extends Function>(fun: T, wait: number) {
+    let id: any;
+    return (...args: any[]) => {
+      clearTimeout(id);
+      id = setTimeout(() => fun(...args), wait);
+    };
+  }
 }
 
 class Utils extends Debouncer {
+  static rand(min: number, max: number, reduce: number = SIZE): number {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num - (num % reduce);
+  }
 
-    static rand(min: number, max: number, reduce: number = SIZE): number {
-        const num = Math.floor(Math.random() * (max - min)) + min;
-        return num - (num % reduce);
-    }
+  static snap(num: number, point = SIZE): number {
+    const bottom = num - (num % point);
+    const top = bottom + point;
 
-    static snap(num: number, point = SIZE): number {
-        const bottom = num - (num % point);
-        const top = bottom + point;
+    return num - bottom <= top - num ? bottom : top;
+  }
 
-        return num - bottom <= top - num ? bottom : top;
-    }
+  /**
+   * FELADAT!
+   * Hozd létre a removeNode nevű statikus metódust!
+   * A metódus a kapott elemet eltávolítja a saját parentNode-jából.
+   * @param el {Element} - egy DOM Element típus
+   * @returns {void}
+   */
 
-    /**
-     * FELADAT!
-     * Hozd létre a removeNode nevű statikus metódust!
-     * A metódus a kapott elemet eltávolítja a saját parentNode-jából.
-     * @param el {Element} - egy DOM Element típus
-     * @returns {void}
-     */
-
-
+  static removeNode(el: Element): void {
+    el.remove();
+  }
 }
 
 export default Utils;
