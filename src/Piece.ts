f286791cb0fa28a01be65c3187e04e1d225dbdb6
k3csk3 <szilagyi.viktor89@gmail.com>
2022-10-23 17:49:05 +0200

    piece done

--- src/Piece.ts
+++ src/Piece.ts
@@ -1,6 +1,6 @@
-import { SIZE } from './constants';
-import Utils from './Utils';
-import Locations from './Locations';
+import { SIZE } from "./constants";
+import Utils from "./Utils";
+import Locations from "./Locations";
 
 interface IPieceParams {
   x: number;
@@ -9,7 +9,7 @@ interface IPieceParams {
   direction?: string;
   next?: Piece | null;
   prev?: Piece | null;
-};
+}
 
 interface IPiece {
   next: Piece | null;
@@ -41,8 +41,8 @@ interface IPiece {
    * Megállapítja, hogy ütközött-e a kígyó darabja valamivel.
    * @param node {Piece}
    * @returns {boolean}
-   * Ha a node falsy, akkor false értékkel tér vissza, 
-   * Ha nem, akkor abban az esetben tér vissza true-val, ha a this.x egyenlő a node.x-szel 
+   * Ha a node falsy, akkor false értékkel tér vissza,
+   * Ha nem, akkor abban az esetben tér vissza true-val, ha a this.x egyenlő a node.x-szel
    * és a this.y egyenlő a node.y-nal.
    */
   isCollidingWith(node: Piece | null): boolean;
@@ -53,14 +53,44 @@ interface IPiece {
  * Pótold a hiányzó tulajdonságokat és metódusokat az interfész alapján.
  */
 export default class Piece implements IPiece {
-  
-  
-  
+  next: Piece | null;
+  prev: Piece | null;
+  x: number;
+  y: number;
+  el: HTMLDivElement;
+  direction: string;
+  type: string;
+  garden: HTMLDivElement;
+
+  setType(type: string): void {
+    this.type = type;
+    this.applyClass();
+  }
+
+  applyClass(): void {
+    this.el.className = "";
+    this.el.classList.add("cell");
+    this.el.classList.add("this.type");
+    this.el.classList.add("this.direction");
+  }
+
+  isCollidingWith(node: Piece | null): boolean {
+    if (node === null) {
+      return false;
+    } else {
+      if (this.x === node.x && this.y === node.y) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
   constructor({
     x,
     y,
-    type = 'body',
-    direction = 'RIGHT',
+    type = "body",
+    direction = "RIGHT",
     next = null,
     prev = null,
   }: IPieceParams) {
@@ -68,23 +98,23 @@ export default class Piece implements IPiece {
     this.type = type;
     this.x = x;
     this.y = y;
-    this.el = document.createElement('div');
+    this.el = document.createElement("div");
     this.next = next;
     this.prev = prev;
     // Enable for a neat effect
     // this.el.innerHTML = "&#10096;";
     this.setType(type);
     this.setPos(this.x, this.y);
-    this.garden = (document.getElementById('garden') as HTMLDivElement);
+    this.garden = document.getElementById("garden") as HTMLDivElement;
     // this.applyClass();
     this.garden.appendChild(this.el);
   }
 
   bend(headDirection: string) {
     if (this.direction !== headDirection) {
-      this.el.className = '';
+      this.el.className = "";
       this.el.classList.add(
-        'cell',
+        "cell",
         this.type,
         headDirection,
         `${headDirection}-${this.direction}`
@@ -107,12 +137,16 @@ export default class Piece implements IPiece {
     // But don't do this, if we are the food or head because;
     // - Head cannot collide with itself
     // - We want to collide with food :)
-    if (this.type !== 'head' && this.type !== 'food' && this.type !== 'golden') {
+    if (
+      this.type !== "head" &&
+      this.type !== "food" &&
+      this.type !== "golden"
+    ) {
       Locations.set(x, y);
     }
   }
 
-  move(x: number, y: number, direction: string = 'RIGHT'): void {
+  move(x: number, y: number, direction: string = "RIGHT"): void {
     let X = x;
     let Y = y;
 
@@ -137,7 +171,11 @@ export default class Piece implements IPiece {
     if (this.next !== null) {
       // If this piece is a head piece, immediate piece should receive heads current
       // direction instead of old one this is needed to have a fluid motion
-      this.next.move(this.x, this.y, this.type === 'head' ? this.direction : tDirection);
+      this.next.move(
+        this.x,
+        this.y,
+        this.type === "head" ? this.direction : tDirection
+      );
     } else {
       // We are the last piece, previous position
       // is now empty, clear it
@@ -146,14 +184,14 @@ export default class Piece implements IPiece {
 
     // if I"m part of body and no one is following me
     // then I must be the tail
-    if (this.next === null && this.type === 'body') {
-      this.el.classList.add('tail');
+    if (this.next === null && this.type === "body") {
+      this.el.classList.add("tail");
     }
 
     // if me and the piece following me are at the same spot
     // then piece following me must be the food we just swallowed
     if (this.next !== null && this.next.x === X && this.next.y === Y) {
-      this.next.el.classList.add('gulp');
+      this.next.el.classList.add("gulp");
     }
 
     // Bendy corners
@@ -165,8 +203,6 @@ export default class Piece implements IPiece {
     this.x = X;
     this.y = Y;
   }
-
-
 
   remove(): void {
     // Remove the piece, it"s location and HTML element
